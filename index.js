4b2e3716cee82a757edae627c913b564417fe090
Mayas <mayas.haddad@save.co>
2017-01-21 12:41:19 +0100

    feat: create logger only in production env

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const bunyan = require('bunyan')
 
 module.exports = (bunyanLoggerOptions) => {
-  if(process.env.NODE_ENV === 'development') {
-    return {
-      error: (err) => { console.log(err) },
-      info: (info) => { console.log(info) },
-      warn: (w) => { console.log(warn) }
-    }
+  if(process.env.NODE_ENV === 'production') return bunyan.createLogger(bunyanLoggerOptions)
+  return {
+    error: (err) => { console.log(err) },
+    info: (info) => { console.log(info) },
+    warn: (w) => { console.log(warn) }
   }
-  return bunyan.createLogger(bunyanLoggerOptions)
 }
